d0bb59075ae00ff70114cdf7c8cc00ebc9c14c63
codywd <dostalcody@gmail.com>
2014-08-27 19:21:09 -0600

    Fix failed git merge.

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@
 from distutils.core import setup
 
 setup(name='netgui',
-<<<<<<< HEAD
-      version='0.8',
-=======
-      version='0.7.1',
->>>>>>> 0.7.1
+    version='0.8',
     description = "More advanced GUI for NetCTL. Replaces WiFiz.",
     author = "Cody Dostal <dostalcody@gmail.com>, Gregory Mullen <greg@grayhatter.com>",
     url = "https://github.com/codywd/netgui",
