fe418e275b58ea62e46ac0dfa884bbe1365159c1
codywd <dostalcody@gmail.com>
2014-09-17 16:51:40 -0600

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='netgui',
-    version='0.8',
+      version='0.7.2',
     description = "More advanced GUI for NetCTL. Replaces WiFiz.",
     author = "Cody Dostal <dostalcody@gmail.com>, Gregory Mullen <greg@grayhatter.com>",
     url = "https://github.com/codywd/netgui",
@@ -14,15 +14,17 @@ setup(name='netgui',
     data_files=[
     ('/usr/share/netgui/',
         ['main.py',
-        'UI.glade',
-        'imgs/APScan.png',
-        'imgs/connect.png',
-        'imgs/exit.png',
-        'imgs/newprofile.png',
-        'imgs/aboutLogo.png',
-        'imgs/disconnect.png',
-        'imgs/logo.png',
-        'imgs/preferences.png' ]),
+        'UI.glade']),
     ('/usr/share/licenses/netgui',
-        ['NetGUI.license'])
+        ['NetGUI.license']),
+    ('/usr/share/negui/imgs',
+        ['APScan.png',
+         'connect.png',
+         'exit.png',
+         'newprofile.png',
+         'aboutLogo.png',
+         'disconnect.png',
+         'logo.png',
+         'preferences.png'])
+
 ])
